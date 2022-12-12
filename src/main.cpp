114a1a4d6fcfd0e515313cab7002d44283a27f0b
Diya Thomas <diyast2@illinois.edu>
2022-12-12 16:58:52 -0600

    fixed test cases

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,6 @@
 int main() {
     // Graph g("data/airports_cleaned_large.csv", "data/adjacency_list_large.csv", "data/distance_matrix_large.csv"); // creates graph from correct data
     Graph g(true); // false for small dataset, large for true
-    // Graph gg(true); // creates graph from scratch, should match correct data
-
-    // gg.writeAdjListToFile("data/adjacency_list_large.csv");
-    // gg.writeAirportsToFile("data/airports_cleaned_large.csv");
-    // gg.writeDistMatrixToFile("data/distance_matrix_large.csv");
-    // std::cout << g.adjlistSize() << std::endl; // should equal 220 (IT DOES)
-    // std::cout << g.airportsSize() << std::endl; // shoudl equal 220 (IT DOES)
-    
-    // should print out same values (THEY DO) because the graphs should be the same
-    // std::cout << g.BFSTraversal(0).size() << std::endl;
-    // std::cout << gg.BFSTraversal(0).size() << std::endl;
-    
-    std::cout << "distance: " << g.airports[0].calculateWeight(g.airports[206]) << std::endl; // distance between two airports
-    std::cout << "distance: " << g.airports[206].calculateWeight(g.airports[114]) << std::endl;
-    std::cout << "To get from " << g.airports[0].airport_name << " to " << g.airports[114].airport_name << ", you fly "
-    <<g.getShortestPath(g.airports[0].airport_id, g.airports[114].airport_id) << " km"<< std::endl; //should equal sum of above 2 lines (IT DOES)
-    // std::cout << "connection is: " << g.airports[206].airport_name << std::endl;
-    // std::cout << gg.getShortestPath(g.airports[0].airport_id, g.airports[114].airport_id) << std::endl; // should be same as line above (IT IS)
-    // Graph g2(true);
-    // std::cout << g2.BFSTraversal(0).size() << std::endl;
 
     std::cout << "\n \n" << "------SHORTEST PATH------" << std::endl;
 
@@ -88,10 +68,10 @@ int main() {
     }
 
     std::cout <<  "---TEST CASE 2---" << std::endl; //
-    std::vector<Vertex> path2 = g.BFSTraversal(345); //takes in index
+    std::vector<Vertex> path2 = g.BFSTraversal(2400); //takes in index
     found = false; 
     for(Vertex a : path2) {
-        if(a.airport_id == 1378) {
+        if(a.airport_id == 1353) {
             found = true; 
         }
     }
@@ -102,7 +82,7 @@ int main() {
     }
 
     std::cout <<  "---TEST CASE 3---" << std::endl; // 0, 3423
-    std::vector<Vertex> path3 = g.BFSTraversal(g.idToIndex(1291)); //takes in index
+    std::vector<Vertex> path3 = g.BFSTraversal(83); //takes in index
     found = false; 
     for(Vertex a : path3) {
         if(a.airport_id == 1330) {
